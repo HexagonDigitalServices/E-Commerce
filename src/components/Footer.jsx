a85b7ace5ac32906f44481e2fa62c3dd9d5e7505
Hexagon Digital Services <hexagonsservices@gmail.com>
2025-02-10 04:22:02 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+❤️
+
+[
+                "Couple Outfits",
+                "Romantic Dresses",
+                "Love-themed Accessories",
+                "Gift Packages",
+                "Date Night Styles",
+              ]
+
+[
+                "Contact Us",
+                "Gift Wrapping",
+                "Romantic Surprises",
+                "Love Notes",
+                "Special Requests",
+              ]
+
+💝
+
+💞
